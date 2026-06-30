fb3d547034e5e980db0ee006cdceeceab3062733
Lucas lopes <lucassantos124578@gmail.com>
2026-06-30 17:59:57 -0300

    feat: removido submodulo e adicionado arquivos do portfolio

---
+++ meu_Portfolio_de_apresentacao/src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+// Importando o seu componente principal
+import Home from './components/Home.vue'
+</script>
+
+<template>
+  <Home />
+</template>
+
+<style>
+/* Reset básico para garantir que o App ocupe 100% da tela */
+#app {
+  width: 100%;
+  max-width: 100% !important;
+  margin: 0 !important;
+  padding: 0 !important;
+  display: block !important;
+}
+</style>
